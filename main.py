55607e9d481a8f16c5143b3b6892e48a2bea9699
Korell0 <korsos.adam97@gmail.com>
2021-12-08 12:44:17 +0100

    probalkozas

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 import csv
- 
-with open('otos.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        nyeroszamok = row[4]
 
 
-        
+def nyeroszamok():
+    
+    with open('otos.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            nyeroszamok = row[4]
+            ered = nyeroszamok.split(";")
+            del ered[0]
+            new_list = []
+            ered = [int(i) for i in ered]
+            new_list.append(sum(ered))
+            print(new_list)
+def legkisebb():
+    with open('otos.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            nyeroszamok = row[4]
+            ered = nyeroszamok.split(";")
+            del ered[0]
+            ered = [int(i) for i in ered]
+            newlist = []
+            newlist.append(ered)
+            print(newlist)
+            
+nyeroszamok()
+
