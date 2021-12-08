74a0dcf56336f2b7ebf9c9efe511b92f0e1c0f25
Korell0 <korsos.adam97@gmail.com>
2021-12-08 14:14:12 +0100

    gyakoriszamok

--- main.py
+++ main.py
@@ -19,6 +19,35 @@ def legkisebbszam():
                 else:
                     kicsi = items
         return kicsi
-legkisebbszam()
+
+def leggyakoribbszamok():
+    with open('otos.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        new_list = []
+        num_counter = {}
+        for row in spamreader:
+            nyeroszamok = row[4]
+            ered = nyeroszamok.split(";")
+            del ered[0]
+            ered = [int(i) for i in ered]
+            new_list.append(ered)
+        for items in new_list:
+            for numbers in items:
+                if numbers > 99:
+                    numbers = ""
+                else:
+                    if numbers in num_counter:
+                        num_counter[numbers] += 1
+                    else:
+                         num_counter[numbers] = 1
+                    gyakori_szamok = sorted(num_counter, key = num_counter.get, reverse = True)
+                    gyakori_value = sorted(num_counter, value = num_counter.get, reverse = True)
+                    top_3 = gyakori_szamok[:3]
+                    top_value = gyakori_value[:3]
+        print(top_3)
+        print(top_value)
+                
+leggyakoribbszamok()
+
 
 
