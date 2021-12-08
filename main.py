e7231684a9b080d14dbb2dde1618fce4a068ac82
Korell0 <korsos.adam97@gmail.com>
2021-12-08 13:21:02 +0100

    legkisebbszam

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import csv
 
 
-def nyeroszamok():
-    
+def legkisebbszam():
     with open('otos.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        new_list = []
+        kicsi = 500
         for row in spamreader:
             nyeroszamok = row[4]
             ered = nyeroszamok.split(";")
             del ered[0]
-            new_list = []
             ered = [int(i) for i in ered]
             new_list.append(sum(ered))
-            print(new_list)
-def legkisebb():
-    with open('otos.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in spamreader:
-            nyeroszamok = row[4]
-            ered = nyeroszamok.split(";")
-            del ered[0]
-            ered = [int(i) for i in ered]
-            newlist = []
-            newlist.append(ered)
-            print(newlist)
-            
-nyeroszamok()
+        for items in new_list:
+            if items < kicsi:
+                if items == 0:
+                    continue
+                else:
+                    kicsi = items
+        return kicsi
+legkisebbszam()
+
 
